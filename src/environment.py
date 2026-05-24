337b77637936a9f7a32926414ec47b33de41653b
Rex0626 <kaitotham@gmail.com>
2026-05-24 21:52:39 +0800

    調整版面和將LiDAR從3條升級成7條

--- src/environment.py
+++ src/environment.py
@@ -8,7 +8,6 @@ import random
 class RobotNavigationEnvGUI(gym.Env):
 
     def __init__(self, render_mode=True):
-
         super(RobotNavigationEnvGUI, self).__init__()
 
         # ==================================================
@@ -17,7 +16,7 @@ class RobotNavigationEnvGUI(gym.Env):
         self.observation_space = spaces.Box(
             low=-10.0,
             high=10.0,
-            shape=(8,),
+            shape=(12,),
             dtype=np.float32
         )
 
@@ -34,7 +33,6 @@ class RobotNavigationEnvGUI(gym.Env):
         # Map Settings
         # ==================================================
         self.map_size = 400
-
         self.linear_vel = 8.0
         self.angular_vel = np.deg2rad(15)
 
@@ -69,33 +67,22 @@ class RobotNavigationEnvGUI(gym.Env):
         # Render
         # ==================================================
         self.render_mode = render_mode
-
         if self.render_mode:
-
             pygame.init()
-
-            self.screen = pygame.display.set_mode(
-                (self.map_size, self.map_size)
-            )
-
-            pygame.display.set_caption(
-                "Level4 Navigation"
-            )
-
+            self.screen = pygame.display.set_mode((self.map_size, self.map_size))
+            pygame.display.set_caption("Level4 Navigation")
             self.clock = pygame.time.Clock()
 
     # ==================================================
     # RESET
     # ==================================================
     def reset(self, seed=None, options=None):
-
         super().reset(seed=seed)
 
         # ==================================================
         # Random Start Position
         # ==================================================
         while True:
-
             candidate_pos = np.array([
                 random.uniform(30, 370),
                 random.uniform(30, 370)
@@ -104,84 +91,54 @@ class RobotNavigationEnvGUI(gym.Env):
             safe = True
 
             for obs in self.obstacles:
-
-                dist = np.linalg.norm(
-                    candidate_pos - obs["pos"]
-                )
-
+                dist = np.linalg.norm(candidate_pos - obs["pos"])
                 if dist < (obs["radius"] + 25):
-
                     safe = False
                     break
 
             if safe:
-
                 self.robot_pos = candidate_pos
                 break
 
         # ==================================================
         # Random Heading
         # ==================================================
-        self.robot_theta = random.uniform(
-            0,
-            2*np.pi
-        )
+        self.robot_theta = random.uniform(0,2*np.pi)
 
         # ==================================================
         # Random Goal
         # ==================================================
         while True:
-
             candidate_target = np.array([
                 random.uniform(30, 370),
                 random.uniform(30, 200)
             ])
 
-            dist_to_robot = np.linalg.norm(
-                candidate_target - self.robot_pos
-            )
-
+            dist_to_robot = np.linalg.norm(candidate_target - self.robot_pos)
             safe = True
 
             for obs in self.obstacles:
-
-                dist = np.linalg.norm(
-                    candidate_target - obs["pos"]
-                )
-
+                dist = np.linalg.norm(candidate_target - obs["pos"])
                 if dist < (obs["radius"] + 25):
-
                     safe = False
                     break
 
             if safe and dist_to_robot > 120:
-
                 self.target_pos = candidate_target
                 break
 
         self.current_step = 0
-
         return self._get_obs(), {}
 
     # ==================================================
     # Directional LiDAR
     # ==================================================
     def cast_lidar_ray(self, angle, max_range=120):
-
         step_size = 4
 
         for dist in range(0, max_range, step_size):
-
-            test_x = (
-                self.robot_pos[0]
-                + dist*np.cos(angle)
-            )
-
-            test_y = (
-                self.robot_pos[1]
-                - dist*np.sin(angle)
-            )
-
+            test_x = (self.robot_pos[0] + dist*np.cos(angle))
+            test_y = (self.robot_pos[1] - dist*np.sin(angle))
             # Wall collision
             if (
                 test_x <= 0 or
@@ -193,12 +150,10 @@ class RobotNavigationEnvGUI(gym.Env):
 
             # Obstacle collision
             for obs in self.obstacles:
-
                 d = np.linalg.norm(
                     np.array([test_x, test_y])
                     - obs["pos"]
                 )
-
                 if d <= obs["radius"]:
                     return dist
 
@@ -208,25 +163,17 @@ class RobotNavigationEnvGUI(gym.Env):
     # Observation
     # ==================================================
     def _get_obs(self):
-
-        dist_to_target = np.linalg.norm(
-            self.robot_pos - self.target_pos
-        )
+        dist_to_target = np.linalg.norm(self.robot_pos - self.target_pos)
 
         # ==================================================
-        # Directional LiDAR
+        # 7-Ray LiDAR
         # ==================================================
-        lidar_left = self.cast_lidar_ray(
-            self.robot_theta + np.deg2rad(45)
-        )
-
-        lidar_front = self.cast_lidar_ray(
-            self.robot_theta
-        )
-
-        lidar_right = self.cast_lidar_ray(
-            self.robot_theta - np.deg2rad(45)
-        )
+        lidar_degrees = [-90, -60, -30, 0, 30, 60, 90]
+        lidar_values = []
+        for deg in lidar_degrees:
+            angle = self.robot_theta + np.deg2rad(deg)
+            dist = self.cast_lidar_ray(angle)
+            lidar_values.append(dist / 400.0)
 
         angle_to_target = np.arctan2(
             self.robot_pos[1] - self.target_pos[1],
@@ -236,17 +183,13 @@ class RobotNavigationEnvGUI(gym.Env):
         if angle_to_target < 0:
             angle_to_target += 2*np.pi
 
-        self.angle_error = (
-            angle_to_target - self.robot_theta
-        )
-
+        self.angle_error = (angle_to_target - self.robot_theta)
         self.angle_error = np.arctan2(
             np.sin(self.angle_error),
             np.cos(self.angle_error)
         )
 
         return np.array([
-
             self.robot_pos[0] / 400.0,
             self.robot_pos[1] / 400.0,
             self.robot_theta / (2*np.pi),
@@ -254,105 +197,62 @@ class RobotNavigationEnvGUI(gym.Env):
             dist_to_target / 400.0,
             self.angle_error / np.pi,
 
-            lidar_left / 400.0,
-            lidar_front / 400.0,
-            lidar_right / 400.0
-
+            *lidar_values
         ], dtype=np.float32)
 
     # ==================================================
     # STEP
     # ==================================================
     def step(self, action):
-
         self.current_step += 1
-
-        prev_dist = np.linalg.norm(
-            self.robot_pos - self.target_pos
-        )
+        prev_dist = np.linalg.norm(self.robot_pos - self.target_pos)
 
         # ==================================================
         # Action
         # ==================================================
         if action == 0:
-
-            self.robot_pos[0] += (
-                self.linear_vel
-                * np.cos(self.robot_theta)
-            )
-
-            self.robot_pos[1] -= (
-                self.linear_vel
-                * np.sin(self.robot_theta)
-            )
-
+            self.robot_pos[0] += (self.linear_vel * np.cos(self.robot_theta))
+            self.robot_pos[1] -= (self.linear_vel * np.sin(self.robot_theta))
         elif action == 1:
-
             self.robot_theta += self.angular_vel
-
         elif action == 2:
-
             self.robot_theta -= self.angular_vel
-
         elif action == 3:
-
             pass
 
         self.robot_theta %= (2*np.pi)
-
         obs = self._get_obs()
 
-        dist_to_target = np.linalg.norm(
-            self.robot_pos - self.target_pos
-        )
-
+        dist_to_target = np.linalg.norm(self.robot_pos - self.target_pos)
         terminated = False
-
-        truncated = (
-            self.current_step >= self.max_steps
-        )
+        truncated = (self.current_step >= self.max_steps)
 
         # ==================================================
         # Reward Shaping
         # ==================================================
         reward = -0.05
-
-        # closer to target
-        reward += (
-            prev_dist - dist_to_target
-        ) * 0.2
-
-        # heading reward
-        reward -= abs(self.angle_error) * 0.05
+        reward += (prev_dist - dist_to_target) * 1.5 # closer to target
+        reward -= abs(self.angle_error) * 0.3 # heading reward
+        heading_reward = np.cos(self.angle_error)
+        reward += heading_reward * 0.5
 
         # ==================================================
         # Goal Reached
         # ==================================================
         if dist_to_target < 15:
-
             reward += 150
-
             terminated = True
-
             print("✨ 成功抵達目標")
 
         # ==================================================
         # Obstacle Collision
         # ==================================================
         for obs_item in self.obstacles:
-
-            dist_to_obs = np.linalg.norm(
-                self.robot_pos - obs_item["pos"]
-            )
-
+            dist_to_obs = np.linalg.norm(self.robot_pos - obs_item["pos"])
             if dist_to_obs < (obs_item["radius"] + 8):
-
                 reward -= 80
-
                 terminated = True
-
                 print("💥 撞上障礙物")
-
                 break
 
         # ==================================================
@@ -366,9 +266,7 @@ class RobotNavigationEnvGUI(gym.Env):
         ):
 
             reward -= 120
-
             terminated = True
-
             print("🧱 撞牆")
 
         # ==================================================
@@ -388,9 +286,7 @@ class RobotNavigationEnvGUI(gym.Env):
         # Handle Events
         # ==================================================
         for event in pygame.event.get():
-
             if event.type == pygame.QUIT:
-
                 pygame.quit()
                 exit()
 
@@ -403,7 +299,6 @@ class RobotNavigationEnvGUI(gym.Env):
         # Draw Obstacles
         # ==================================================
         for obs in self.obstacles:
-
             pygame.draw.circle(
                 self.screen,
                 (255,0,0),
@@ -435,16 +330,8 @@ class RobotNavigationEnvGUI(gym.Env):
         # Draw Heading
         # ==================================================
         line_len = 15
-
-        end_x = (
-            self.robot_pos[0]
-            + line_len*np.cos(self.robot_theta)
-        )
-
-        end_y = (
-            self.robot_pos[1]
-            - line_len*np.sin(self.robot_theta)
-        )
+        end_x = (self.robot_pos[0] + line_len*np.cos(self.robot_theta))
+        end_y = (self.robot_pos[1] - line_len*np.sin(self.robot_theta))
 
         pygame.draw.line(
             self.screen,
@@ -457,45 +344,26 @@ class RobotNavigationEnvGUI(gym.Env):
         # ==================================================
         # Draw LiDAR Rays
         # ==================================================
+        lidar_degrees = [-90, -60, -30, 0, 30, 60, 90]
         lidar_angles = [
-
-            self.robot_theta + np.deg2rad(45),
-            self.robot_theta,
-            self.robot_theta - np.deg2rad(45)
+            self.robot_theta + np.deg2rad(deg)
+            for deg in lidar_degrees
         ]
 
         lidar_dists = [
-
             self.cast_lidar_ray(angle)
-
             for angle in lidar_angles
         ]
 
-        for angle, dist in zip(
-            lidar_angles,
-            lidar_dists
-        ):
-
-            end_x = (
-                self.robot_pos[0]
-                + dist*np.cos(angle)
-            )
-
-            end_y = (
-                self.robot_pos[1]
-                - dist*np.sin(angle)
-            )
+        for angle, dist in zip(lidar_angles,lidar_dists):
+            end_x = (self.robot_pos[0] + dist*np.cos(angle))
+            end_y = (self.robot_pos[1] - dist*np.sin(angle))
 
             pygame.draw.line(
-
                 self.screen,
-
                 (255,165,0),
-
                 self.robot_pos.astype(int),
-
                 (int(end_x), int(end_y)),
-
                 2
             )
 
@@ -513,6 +381,5 @@ class RobotNavigationEnvGUI(gym.Env):
     # Close
     # ==================================================
     def close(self):
-
         if self.render_mode:
             pygame.quit()
